f29b2aca0469daa2ccc2bdbc8d81e3f207f2ea85
Jordan Tran <thai.jordan@gmail.com>
2023-05-04 14:52:48 -0700

    added icons for users and search bar, playing around tailwindcss

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#c084fc"
+      }
+    },
   },
   plugins: [],
 }
