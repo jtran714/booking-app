42e9b3aff4f7f37d4d0c88c60dedb398f7a1bbb2
Jordan Tran <thai.jordan@gmail.com>
2023-05-04 16:52:07 -0700

    fixed tailwind unknown error

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -4,7 +4,7 @@ export default function LoginPage() {
   return (
     <div className="mt-4">
         <h1 className="text-4xl text-center">Login</h1>
-        <form className="max-w-2x1 mx-auto border">
+        <form className="max-w-md mx-auto">
             <input type ="email" placeholder={'your@email.com'}/>
             <input type="password" placeholder="password"/>
             <button>Login</button>
