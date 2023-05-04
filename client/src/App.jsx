109cfe7ff4c508869cb4a9e4b76741cae2ccde33
Jordan Tran <thai.jordan@gmail.com>
2023-05-04 16:03:22 -0700

    organized headers layouts and index components for now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
+import Layout from "./Layout";
 
 
 export default function App() {
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
     </Routes>
   );
 }
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export default function LoginPage() {
   return (
-    <div></div>
-  )
-}
+    <div className="mt-4">
+        <h1 className="text-4xl text-center">Login</h1>
+        <form className="max-w-2x1 mx-auto border">
+            <input type ="email" placeholder={'your@email.com'}/>
+            <input type="password" placeholder="password"/>
+            <button>Login</button>
+        </form>
+    </div>
+  );
+};
